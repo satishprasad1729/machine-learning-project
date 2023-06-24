cbd900def3393668f2d31b135443c5e54d0e6840
Your NameSatish <satishprasad1729@gmail.com>
2023-06-24 17:23:06 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return 'CI CD pipeline created to deploy app'
+    return 'CI CD pipeline created '
 
 if __name__=='__main__':
     app.run(debug=True)

4c610ac124f49f0f347c89c773fc77c3e0979b90
Your NameSatish <satishprasad1729@gmail.com>
2023-06-24 17:19:09 +0530

    updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return 'Start Machine Learning project'
+    return 'CI CD pipeline created to deploy app'
 
 if __name__=='__main__':
     app.run(debug=True)

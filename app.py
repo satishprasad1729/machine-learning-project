bac6f2dbe09e2f4d1f2038bc064893812389a6ed
Your NameSatish <satishprasad1729@gmail.com>
2023-07-01 18:55:19 +0530

    new changes today

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 
 from flask import Flask
+from housing.logger import logging
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
+    logging.info('we are testing logging module')
     return 'Ready to start machine learning project '
 
 if __name__=='__main__':
